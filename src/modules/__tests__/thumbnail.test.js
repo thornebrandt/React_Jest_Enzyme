23fa81c6dbb1cb96539cacd9a3d7529961482577
Thorne Brandt <thornebrandt@gmail.com>
2017-02-16 18:59:25 -0600

    added animations, stubbed beforeClose

--- src/modules/__tests__/thumbnail.test.js
+++ src/modules/__tests__/thumbnail.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow, mount, render } from 'enzyme';
-import { spy } from 'sinon';
+import { spy, stub } from 'sinon';
 import Portal from 'react-portal';
 import fakePhotos from '../__data__/photos.fake';
 import toJson from 'enzyme-to-json';
@@ -10,18 +10,39 @@ import Thumbnail from '../thumbnail';
 describe('<Thumbnail />', () => {
 	let wrapper, instance, cheerio;
 	let onImgClickSpy, openPortalSpy, closePortalSpy;
+	//jest.useFakeTimers();
 
+	const afterPromises = (done, fn) => {
+	  setTimeout(() => {
+	    try {
+	      fn();
+	      done();
+	    } catch(e) {
+	      done.fail(e);
+	    }
+	  }, 300);
+	}
+
+	beforeAll(() => {
+		window.requestAnimationFrame = window.requestAnimationFrame
+		    || window.mozRequestAnimationFrame
+		    || window.webkitRequestAnimationFrame
+		    || window.msRequestAnimationFrame
+		    || function(f){return setTimeout(f, 1000/60)} // simulate calling code 60
+	});
 
 	beforeEach(() => {
 		onImgClickSpy = spy(Thumbnail.prototype, 'onImgClick');
 		openPortalSpy = spy(Portal.prototype, 'openPortal');
 		closePortalSpy = spy(Portal.prototype, 'closePortal');
+		stub(Thumbnail.prototype, "beforeClose", function(node, close){ close(); });
 		wrapper = mount(<Thumbnail photo={fakePhotos[0]} />);
 		instance = wrapper.instance();
 	});
 
 	afterEach(() => {
 		Thumbnail.prototype.onImgClick.restore();
+		Thumbnail.prototype.beforeClose.restore();
 		Portal.prototype.openPortal.restore();
 		Portal.prototype.closePortal.restore();
 	});
@@ -39,6 +60,7 @@ describe('<Thumbnail />', () => {
 	});
 
 	it('closes portal on bg click', () => {
+		wrapper.find('Portal').props = {};
 		expect(wrapper.ref('portalBG').length).toBe(0);
 		wrapper.find('img').simulate('click');
 		expect(wrapper.ref('portalBG').length).toBe(1);
--- src/modules/thumbnail.js
+++ src/modules/thumbnail.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Portal from 'react-portal';
 import PhotoModal from './photo-modal';
+import TWEEN from 'tween.js';
 
 class Thumbnail extends React.Component{
 	constructor(props){
@@ -22,12 +23,46 @@ class Thumbnail extends React.Component{
 		}
 	}
 
+	onOpen(node){
+		new TWEEN.Tween({ opacity: 0 })
+			.to({ opacity: 1}, 300)
+			.easing(TWEEN.Easing.Cubic.Out)
+			.onUpdate(function(){
+				node.style.opacity = this.opacity;
+			})
+			.start();
+	}
+
+	beforeClose(node, close){
+		new TWEEN.Tween({ opacity: 1 })
+			.to({ opacity: 0 }, 300)
+			.easing(TWEEN.Easing.Cubic.In)
+			.onUpdate(function(){
+				node.style.opacity = this.opacity;
+			})
+			.onComplete(close)
+			.start();
+	}
+
 	render(){
+		function animate(time) {
+			window.requestAnimationFrame(animate);
+			TWEEN.update(time);
+		}
+
+		try{
+			window.requestAnimationFrame(animate);
+		}catch(e){
+			console.log("no requestAnimationFrame");
+		}
+
 		return(
 			<div className="thumbnail">
 				<img onClick={this.onImgClick} src={this.state.thumbnail} />
 				<Portal
 					ref="portal"
+					onOpen={this.onOpen}
+					beforeClose={this.beforeClose}
 					closeOnEsc
 					closeOnOutsideClick
 				>
